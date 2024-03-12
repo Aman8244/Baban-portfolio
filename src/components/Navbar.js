241e78babfc618400a3f16c7f19edba13812a512
Aman Singh <amansinghmarch16@gmail.com>
2024-03-12 22:47:12 +0530

    portfolio initial version

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Disclosure } from "@headlessui/react";
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
     return (
         <>
             <div id="navbar" className="min-h-full sticky top-0 z-50 shadow-md border-b-2 border-gray-400">
@@ -26,7 +25,6 @@ const Navbar = () => {
                             <div className="-mr-2 flex md:hidden">
                                 {/* Headless UI Disclosure Button */}
                                 <Disclosure.Button className="text-gray-400 hover:text-white focus:outline-none">
-                                    {isOpen ? (
                                         <svg
                                             xmlns="http://www.w3.org/2000/svg"
                                             fill="none"
@@ -41,22 +39,6 @@ const Navbar = () => {
                                                 d="M6 18L18 6M6 6l12 12"
                                             />
                                         </svg>
-                                    ) : (
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            stroke="currentColor"
-                                            className="block h-6 w-6"
-                                        >
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="2"
-                                                d="M4 6h16M4 12h16m-7 6h7"
-                                            />
-                                        </svg>
-                                    )}
                                 </Disclosure.Button>
                             </div>
                         </div>
